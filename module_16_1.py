aef0c6e19f3a206bee492dc1ae38dd9692c5d79c
xdvt1337 <76076932+xdvt1337@users.noreply.github.com>
2024-12-14 20:29:59 +0700

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -3,17 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/')
-async def Get_Main_Page() -> dict:
-    return {'message': 'Главная страница'}
+async def Get_Main_Page() -> str:
+    return 'Главная страница'
 
 @app.get('/user/admin')
-async def Get_Admin_Page() -> dict:
-    return {'message': 'Вы вошли как администратор'}
+async def Get_Admin_Page() -> str:
+    return 'Вы вошли как администратор'
 
 @app.get('/user/{user_id}')
-async def Get_User_Number(user_id: int ) -> dict:
-    return {'message': f'Вы вошли как пользователь № {user_id}'}
+async def Get_User_Number(user_id: int) -> str:
+    return f'Вы вошли как пользователь № {user_id}'
 
 @app.get('/user')
-async def Get_User_Info(username: str, age: int) -> dict:
-    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
+async def Get_User_Info(username: str, age: int) -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
